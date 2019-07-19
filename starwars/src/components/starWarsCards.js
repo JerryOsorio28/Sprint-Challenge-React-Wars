09f0819bc9c6ed91ba048244a1f0e7988059481c
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-07-19 19:25:29 -0400

    created styling js file and moved styling to it

--- starwars/src/components/starWarsCards.js
+++ starwars/src/components/starWarsCards.js
@@ -1,33 +1,7 @@
 import React from "react";
-import styled from 'styled-components';
+import {CardsStyles, NameStyles, AttributesStyles} from './cardsStyle';
 
 export default function StarWarsCards (props) {
-    
-    const CardsStyles = styled.div`
-        border: 1px solid black;
-        width: 50%;
-        margin-left: 25%;
-        margin-bottom: 1%;
-    `;
-    const NameStyles = styled.h1`
-        border: 1px solid black;
-        background-color: grey;
-        margin: 0;
-        padding: 3%;
-        text-shadow: 5px 2px 5px black;
-        color: yellow;
-        
-    `;
-    const AttributesStyles = styled.h5`
-        border: 1px solid black;
-        background-color: grey;
-        margin: 0;
-        padding: 10% 3%;
-        text-shadow: 1px 1px 5px black;
-        color: white;
-        text-align: right;
-
-    `;
 
     return (
       <CardsStyles>
